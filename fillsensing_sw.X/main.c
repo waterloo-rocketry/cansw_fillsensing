15319d8a0bb5f6686e0e723d44900dcafd730e28
Wendi <wendi.yu@yahoo.ca>
2020-07-19 15:07:17 -0600

    Clean code

--- fillsensing_sw.X/main.c
+++ fillsensing_sw.X/main.c
@@ -16,19 +16,16 @@
 #define HALL_13 IOCCF6
 #define HALL_14 IOCCF4
 
-// QOL led macros
+// QOL LED macros
 #define WHITE LATA3
 #define BLUE LATA4
 #define RED LATA5
 
-typedef unsigned char uint8_t;
-
-#include <stdio.h>
 #include <stdlib.h>
 #include "mcc_generated_files/mcc.h"
 
 // variable that stores current fill level
-volatile uint8_t fill_level = 92;
+volatile uint8_t fill_level = 0;
 
 // function to test board; leds should flash for 1s each and cycle through
 
@@ -47,12 +44,16 @@ void leds() {
 }
 
 void pin_init() {
+    // set RA as an output port (for LEDs)
+    TRISA = 0b00000000;
+
+
     // set RB and RC as input ports
     TRISB = 0b11111111;
     TRISC = 0b11111111;
 
 
-    // enable interrupt-on-change for RB and RC on rising edge
+    // enable interrupt-on-change for RB and RC on falling edge
     IOCBN = 0b11111111;
     IOCCN = 0b11111111;
 
@@ -61,19 +62,22 @@ void pin_init() {
     ANSELB = 0b00000000;
     ANSELC = 0b00000000;
 
+
     // clear flags (just in case)
     IOCCF = 0b00000000;
     IOCBF = 0b00000000;
-
 }
 
 void update_fill(volatile uint8_t* fill, uint8_t new_fill) {
+    // indicator LED
+    RED = RED == 1 ? 0 : 1;
+
     // update the fill level
     *fill = new_fill;
 }
 
 static void __interrupt() interrupt_handler() {
-    // which sensor went off, update accordingly
+    // check which sensor went off, update accordingly
     if (HALL_1) {
         HALL_1 = 0;
         update_fill(&fill_level, 1);
@@ -123,22 +127,17 @@ int main(int argc, char** argv) {
     // enable interrupt-on-change
     PIE0bits.IOCIE = 1;
 
-    // initialize IO for pins
+    // initialize pins, interrupts
     pin_init();
 
     // enable global interrupts
     INTCON0bits.GIE = 1;
 
-    //for LED testing
-    TRISA = 0b00000000;
-    WHITE = 1;
-    BLUE = 1;
-    RED = 1;
-
     fill_level = 0;
 
     // keep the board running and listening for interrupts
     while (true) {
+        // heartbeat LED
         BLUE = 0;
         __delay_ms(1000);
         BLUE = 1;
